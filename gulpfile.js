9d31b229b802cb45e2f9752a6da3a2bcc14e9fab
leon.shi <superRaytin@163.com>
2015-12-24 12:44:13 +0800

    grunt -> gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+﻿var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var browserify = require('gulp-browserify');
+var rename = require("gulp-rename");
+var banner = require("gulp-banner");
+var pkg = require('./package.json');
+
+var comment = '/*\n' +
+    ' * <%= pkg.name %> <%= pkg.version %>\n' +
+    ' * <%= pkg.description %>\n' +
+    ' * <%= pkg.homepage %>\n' +
+    ' *\n' +
+    ' * Copyright 2015, <%= pkg.author %>\n' +
+    ' * Released under the <%= pkg.license %> license.\n' +
+    '*/\n\n';
+
+gulp.task('build', function() {
+    gulp.src('lib/index.js')
+        .pipe(browserify({
+            ignore: ['fs', 'buffer']
+        }))
+        .pipe(uglify())
+        .pipe(banner(comment, {
+            pkg: pkg
+        }))
+        .pipe(rename('imageClipper.js'))
+        .pipe(gulp.dest('dist'));
+});
